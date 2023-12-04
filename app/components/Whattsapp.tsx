a21e8d74b0c283f9cc96e714e78e1d1e6af32da8
DevUttesh <developeruttesh@gmail.com>
2023-12-04 15:51:53 -0800

    error solved

--- app/components/Whattsapp.tsx
+++ app/components/Whattsapp.tsx
@@ -6,6 +6,6 @@ import img from '../public/whattsapp.png'
 export default function App() {
 
   return (
-      <FloatingWhatsApp avatar={img.src} phoneNumber='918128134195' className='text-black' chatMessage='Hello ! How Can I Help You !'  notificationSound={true} allowEsc={true} statusMessage='Dm any time'  accountName='Suthar Uttesh' avt />
+      <FloatingWhatsApp avatar={img.src} phoneNumber='918128134195' className='text-black' chatMessage='Message to get your website !'  notificationSound={true} allowEsc={true} statusMessage='Dm any time'  accountName='Suthar Uttesh' />
   )
 }
